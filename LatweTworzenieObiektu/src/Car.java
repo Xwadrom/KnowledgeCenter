4b540bba9c8b419cbde1fc8a7b5b3d346f9878a0
Robert Osuch <50024773+Xwadrom@users.noreply.github.com>
2019-09-06 16:07:57 +0200

    Add files via upload

---
+++ LatweTworzenieObiektu/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    int year;
+    String brand;
+    String model;
+    String color;
+
+    Car(int y, String b, String m, String c){
+        year=y;
+        brand=b;
+        model=m;
+        color=c;
+    }
+}
---
+++ LatweTworzenieObiektu/src/CarShop.java
@@ -0,0 +1,7 @@
+public class CarShop {
+    public static void main(String[] args) {
+        Car car1= new Car(1990, "BMW", "E96", "Black" );
+        System.out.println(car1.model+" "+car1.color);
+    }
+
+}
